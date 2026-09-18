d0c7f1b5f6d7fb1180be0e7fa7a93cb4a802a974
TheGovernor-Krishna <tejas.25bce11315@vitbhopal.ac.in>
2026-09-18 20:34:43 +0530

    Add files via upload

---
+++ src/com/NovaBank/util/IdGenerator.java
@@ -0,0 +1,28 @@
+package com.novabank.util;
+
+public class IdGenerator {
+
+    private static int customerCounter = 1;
+    private static int accountCounter = 100001;
+
+    public static void setCustomerCounter(int value) {
+        customerCounter = value;
+    }
+
+    public static void setAccountCounter(int value) {
+        accountCounter = value;
+    }
+
+    public static String generateCustomerId() {
+
+        return "C" + String.format("%03d", customerCounter++);
+
+    }
+
+    public static String generateAccountNumber() {
+
+        return "SB" + accountCounter++;
+
+    }
+
+}
